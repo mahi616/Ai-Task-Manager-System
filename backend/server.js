dc10342a9f0d169d837ca50a079370f17a00c111
Mohit Jodhe <mohitjodhe@gmail.com>
2026-01-15 20:32:35 +0530

    Configure CORS to allow requests from the production frontend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://aitaskmanagersystem.vercel.app",
+    methods:['GET','POST','PUT','DELETE'],
+    credentials: true
+}));
 app.use(express.json()); // Body parser
 
 // Routes
